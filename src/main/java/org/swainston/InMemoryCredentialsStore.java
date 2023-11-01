3cd6cd663dcddb0967fca53e5a5f4022c790e321
Tom <tom.j.swainston@gmail.com>
2023-11-01 17:12:38 +0000

    Additional code annotation

--- src/main/java/org/swainston/InMemoryCredentialsStore.java
+++ src/main/java/org/swainston/InMemoryCredentialsStore.java
@@ -3,14 +3,19 @@ package org.swainston;
 import java.util.HashMap;
 import java.util.Map;
 
+/**
+ * This class is only used for local testing prior to the implementation of a database.
+ *
+ * @see org.swainston.database.MySQLCredentialsStore
+ */
 public class InMemoryCredentialsStore implements CredentialsStore {
 
-  private final Map<String, String> map = new HashMap<>();
+  private final Map<String, String> credentialMap = new HashMap<>();
 
   @Override
   public boolean add(String email, String password) {
     if (!exists(email)) {
-      map.put(email, password);
+      credentialMap.put(email, password);
       return true;
     } else {
       return false;
@@ -19,11 +24,11 @@ public class InMemoryCredentialsStore implements CredentialsStore {
 
   @Override
   public boolean exists(String email) {
-    return map.containsKey(email);
+    return credentialMap.containsKey(email);
   }
 
   @Override
   public boolean validate(String email, String password) {
-    return (map.containsKey(email) && map.get(email).equals(password));
+    return (credentialMap.containsKey(email) && credentialMap.get(email).equals(password));
   }
 }
--- src/main/java/org/swainston/SignInSession.java
+++ src/main/java/org/swainston/SignInSession.java
@@ -6,48 +6,70 @@ import org.apache.wicket.request.Request;
 import org.swainston.database.MySQLCredentialsStore;
 
 public final class SignInSession extends AuthenticatedWebSession {
-  private final CredentialsStore credentialsStore = new MySQLCredentialsStore();
-  /**
-   * Trivial user representation
-   */
-  private String user;
 
-  SignInSession(Request request) {
+  // Credential store is set to desired store
+  private final CredentialsStore credentialsStore = new InMemoryCredentialsStore();
+
+  private String username;
+
+  /**
+   * Constructing method that is run when the class is called
+   * @param request the request object (as per apache javadoc)
+   */
+  public SignInSession(Request request) {
     super(request);
+    // Credentials that always exist to allow for testing
     if (!credentialsStore.exists("foo")) {
       credentialsStore.add("foo", "bar");
     }
   }
 
+  /**
+   * Trivial sign out method
+   */
   @Override
   public void signOut() {
     super.signOut();
-
   }
 
   /**
    * Authenticates the given username and password.
    *
-   * @param username The username
-   * @param password The password
-   * @return True if the user was authenticated
+   * @param username the username
+   * @param password the password
+   *
+   * @return true if the user was authenticated
    */
   @Override
   public boolean authenticate(final String username, final String password) {
 
     if (credentialsStore.validate(username, password)) {
-      user = username;
+      this.username = username;
     } else {
-      user = null;
+      this.username = null;
     }
-
-    return user != null;
+    return this.username != null;
   }
 
+  /**
+   * Validates whether a user exists
+   *
+   * @param username the username
+   *
+   * @return whether the user exists
+   */
   public boolean userExists(String username) {
     return credentialsStore.exists(username);
   }
 
+  /**
+   * Creates a new user in the credential store
+   *
+   * @param username the validated username
+   * @param password the password
+   *
+   * @return whether the sign-up was a success
+   */
   public boolean signUp(final String username, final String password) {
     if (!userExists(username)) {
       return credentialsStore.add(username, password);
@@ -57,14 +79,14 @@ public final class SignInSession extends AuthenticatedWebSession {
   }
 
   public String getUser() {
-    return user;
+    return username;
   }
 
   /**
    * @param user New user
    */
   public void setUser(final String user) {
-    this.user = user;
+    this.username = user;
   }
 
   @Override
